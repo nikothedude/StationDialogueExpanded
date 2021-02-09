8b6a1f925b4dd8c808807dc076f05af2ec1bd423
nikothedude <59709059+nikothedude@users.noreply.github.com>
2021-02-09 11:03:30 -0500

    more

--- jars/src/com/fs/starfarer/api/impl/campaign/rulecmd/SDE_determineStealableCommodities.java
+++ jars/src/com/fs/starfarer/api/impl/campaign/rulecmd/SDE_determineStealableCommodities.java
@@ -13,6 +13,7 @@ import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.combat.MutableStat;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.util.Misc;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 import java.util.Map;
@@ -25,6 +26,9 @@ public class SDE_determineStealableCommodities extends BaseCommandPlugin {
     protected MemoryAPI memory;
     protected SectorEntityToken target;
 
+    public static Logger log = Global.getLogger(SDE_determineStealableCommodities.class);
+
+
     @Override
     public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
 
@@ -32,11 +36,8 @@ public class SDE_determineStealableCommodities extends BaseCommandPlugin {
 
 
         float marketStab = dialog.getInteractionTarget().getMarket().getStabilityValue();
-    //    target = dialog.getInteractionTarget();
-   //     MarketAPI market = target.getMarket();
-        //       MarketDemandDataAPI demandData = Global.getSector().getEconomy().getMarket(String.valueOf(market)).getDemandData();
-        int demandData = dialog.getInteractionTarget().getMarket().getCommodityData(SUPPLIES).getDeficitQuantity();
-       // float demandData = (float) demandData1;
+        int demandDeficit = dialog.getInteractionTarget().getMarket().getCommodityData(SUPPLIES).getDeficitQuantity();
+        int commoditySurplus = dialog.getInteractionTarget().getMarket().getCommodityData(SUPPLIES).getExcessQuantity();
         float demandDataValue = dialog.getInteractionTarget().getMarket().getDemand(SUPPLIES).getDemandValue();
 
         memory = getEntityMemory(memoryMap);
@@ -50,12 +51,14 @@ public class SDE_determineStealableCommodities extends BaseCommandPlugin {
                 return false;
 
         if (command.equals("marketExploitableByInsider")) {
-            if (marketStab <= 5) //placeholder value for debugging {
-                memory.set("$SDE_demandData", demandData, 0);
-                memory.set("$SDE_demandDataValue", demandDataValue, 0);
-                return true;
+            if (marketStab >= 5) {//placeholder value for debugging {
+//               log.info("deficit is " + demandDeficit); //this doesnt even log it. it just makes the code WORK.
+                memoryMap.get(MemKeys.MARKET).set("$SDE_demandDeficit", demandDeficit, 0);
+                memoryMap.get(MemKeys.MARKET).set("$SDE_commoditySurplus", commoditySurplus, 0);
+                memoryMap.get(MemKeys.MARKET).set("$SDE_demandDataValue", demandDataValue, 0);
+            }
+            return true;
             }
             else return false;
-
     }
 }
---
+++ jars/src/com/fs/starfarer/api/impl/campaign/rulecmd/SDE_findHostilePresence.java
@@ -0,0 +1,92 @@
+package com.fs.starfarer.api.impl.campaign.rulecmd;
+
+import com.fs.starfarer.api.campaign.FactionAPI;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.SectorEntityToken;
+import com.fs.starfarer.api.campaign.StarSystemAPI;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.characters.PersonAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Conditions;
+import com.fs.starfarer.api.impl.campaign.intel.bases.*;
+import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.api.impl.campaign.econ.BaseMarketConditionPlugin;
+import com.fs.starfarer.api.impl.campaign.intel.bases.LuddicPathCellsIntel;
+import com.fs.starfarer.api.impl.campaign.intel.bases.LuddicPathBaseIntel;
+
+
+
+import java.util.List;
+import java.util.Map;
+
+import static com.fs.starfarer.api.impl.campaign.ids.Conditions.PATHER_CELLS;
+import static com.fs.starfarer.api.impl.campaign.ids.Conditions.PIRATE_ACTIVITY;
+import static com.fs.starfarer.api.impl.campaign.intel.bases.PirateBaseIntel.PirateBaseTier;
+import static com.fs.starfarer.api.impl.campaign.intel.bases.PirateBaseIntel.PirateBaseTier.*;
+
+
+public class SDE_findHostilePresence extends BaseCommandPlugin {
+
+    protected StarSystemAPI system;
+    protected MarketAPI market;
+    protected PersonAPI person;
+    protected MemoryAPI memory;
+    protected SectorEntityToken target;
+    protected PirateBaseIntel source;
+    protected PirateBaseTier tier;
+    protected String factionId;
+    protected LuddicPathCellsIntel intel;
+
+
+
+    @Override
+    public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+
+        String command = params.get(0).getString(memoryMap);
+
+        market = dialog.getInteractionTarget().getMarket();
+        person = dialog.getInteractionTarget().getActivePerson();
+        boolean isLuddicCellPresent = dialog.getInteractionTarget().getMarket().hasCondition(PATHER_CELLS);
+        boolean doesSystemHavePirateActivity = dialog.getInteractionTarget().getMarket().hasCondition(PIRATE_ACTIVITY);
+
+        PirateBaseIntel originTierPirate = new PirateBaseIntel(system, factionId, tier);
+        PirateBaseTier getPirateBaseTier = originTierPirate.getTier();
+
+
+        if (!doesSystemHavePirateActivity && !isLuddicCellPresent)
+            return false;
+
+        if (command.equals("hasPatherCells")) {
+            return isLuddicCellPresent;
+        }
+
+        if (command.equals("hasPirateActivity")) {
+            return doesSystemHavePirateActivity;
+        }
+
+        switch (command) {
+
+            case "isBaseTierOne":
+                return (getPirateBaseTier.equals(TIER_1_1MODULE));
+
+            case "isBaseTierTwo":
+                return (getPirateBaseTier.equals(TIER_2_1MODULE));
+
+            case "isBaseTierThree":
+                return (getPirateBaseTier.equals(TIER_3_2MODULE));
+
+            case "isBaseTierFour":
+                return (getPirateBaseTier.equals(TIER_4_3MODULE));
+
+            case "isBaseTierFive":
+                return (getPirateBaseTier.equals(TIER_5_3MODULE));
+
+            default:
+                return false;
+
+        }
+
+
+    }
+}
